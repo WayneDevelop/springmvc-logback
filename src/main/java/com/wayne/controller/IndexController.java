4904093dc2022031ca7069bd799e68a334a8b368
WayneDevelop <2422014433@qq.com>
2019-11-10 12:16:52 +0800

    新增 logback日志模块

--- src/main/java/com/wayne/controller/IndexController.java
+++ src/main/java/com/wayne/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.wayne.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping( value = "/")
     public String  index(){
+        logger.info("1111111111111");
+        logger.error("22222222222");
         return "home";
     }
 }
